66dcc01c20205167a39e40a8cbeb46d53b315743
pris-matic <deocadizjranthony@gmail.com>
2026-02-26 02:44:12 +0800

    attatched mini explosions

--- bullet.gd
+++ bullet.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 @export var speed := 150.0
 @export var damage := 1
+@export var hit_explosion_scene: PackedScene
 var direction: Vector2 = Vector2.UP
 
 func _ready() -> void:
-	monitoring = true # needed for detection signals [web:700]
+	monitoring = true # needed for detection signals
 
 func set_direction(dir: Vector2) -> void:
 	direction = dir.normalized()
@@ -20,9 +21,16 @@ func _physics_process(delta: float) -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-# Connect: Area2D -> body_entered(body) -> _on_body_entered
 func _on_body_entered(body: Node2D) -> void:
-	_try_hit(body)
+	if body.is_in_group("enemy") and body.has_method("apply_damage"):
+		body.apply_damage(damage)
+
+		if hit_explosion_scene != null:
+			var ex := hit_explosion_scene.instantiate()
+			get_tree().current_scene.add_child(ex)
+			ex.global_position = global_position
+
+		queue_free()
 
 func _try_hit(body: Node) -> void:
 	if body == null:
